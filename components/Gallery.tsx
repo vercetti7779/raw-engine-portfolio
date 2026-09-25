53eafeed307183f2d4f610545e1b34dc02440e7d
Afnan <appuafudoz258@gmail.com>
2026-09-25 21:11:46 +0530

    Improved hero section and contact page

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -3,18 +3,33 @@
 import { useState } from "react";
 import Lightbox from "./Lightbox";
 
-const posters = [
-  "p1.jpeg",
-  "p2.jpeg",
-  "p3.jpeg",
-  "p4.jpeg",
-  "p5.jpeg",
-  "p6.png",
-  "p7.png",
+const portfolio: { type: "image" | "video"; file: string }[] = [
+  { type: "image", file: "p1.jpeg" },
+  { type: "image", file: "p5.jpeg" },
+  { type: "image", file: "p3.jpeg" },
+  { type: "image", file: "p4.jpeg" },
+  { type: "image", file: "p12.jpeg" },
+  { type: "image", file: "p6.png" },
+  { type: "image", file: "p7.png" },
+  { type: "image", file: "p8.png" },
+  { type: "image", file: "p9.jpeg" },
+  { type: "image", file: "p10.png" },
+  { type: "image", file: "p11.jpeg" },
+  { type: "image", file: "p2.jpeg" },
+  { type: "image", file: "p13.png" },
+
+  { type: "video", file: "v1.mp4" },
+  { type: "video", file: "v2.mp4" },
 ];
 
 export default function Gallery() {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [filter, setFilter] = useState<"all" | "image" | "video">("all");
+
+  const filteredPortfolio =
+  filter === "all"
+    ? portfolio
+    : portfolio.filter((item) => item.type === filter);
 
   return (
     <>
@@ -34,19 +49,35 @@ export default function Gallery() {
 
         <div className="columns-1 md:columns-2 lg:columns-3 gap-6 space-y-6">
 
-          {posters.map((poster) => (
-            <div
-              key={poster}
-              onClick={() => setSelectedImage(poster)}
-              className="overflow-hidden rounded-2xl bg-neutral-900 break-inside-avoid group cursor-pointer"
-            >
-              <img
-                src={`/works/${poster}`}
-                alt={poster}
-                className="w-full transition duration-500 group-hover:scale-105"
-              />
-            </div>
-          ))}
+          {filteredPortfolio.map((item) => (
+  <div
+    key={item.file}
+    onClick={() => {
+      if (item.type === "image") {
+        setSelectedImage(item.file);
+      }
+    }}
+    className="overflow-hidden rounded-2xl bg-neutral-900 break-inside-avoid group cursor-pointer"
+  >
+    {item.type === "image" ? (
+      <img
+        src={`/works/${item.file}`}
+        alt={item.file}
+        className="w-full transition duration-500 group-hover:scale-105"
+      />
+    ) : (
+      <video
+        src={`/works/${item.file}`}
+        autoPlay
+        muted
+        loop
+        playsInline
+        preload="metadata"
+        className="w-full transition duration-500 group-hover:scale-105"
+      />
+    )}
+  </div>
+))}
 
         </div>
       </section>
